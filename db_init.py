671b619ec1d94ef5406aeb0ecb701d39e40e5d7d
aaron <esposito.aaron93@gmail.com>
2025-03-15 14:40:54 -0400

    sql integration is functional

---
+++ db_init.py
@@ -0,0 +1,53 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+def initialize_conn():
+
+    load_dotenv()
+
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST", "postgres")
+    DB_PORT = os.getenv("DB_PORT", "5432")
+
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+
+    return conn
+
+def initialize_database():
+
+    conn = initialize_conn()
+    cur = conn.cursor()
+    cur.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS "day" (
+            "date" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "mood" INTEGER NOT NULL,
+            "sleep" REAL NOT NULL,
+            "note" TEXT NOT NULL DEFAULT ' ',
+            "focus" INTEGER NOT NULL DEFAULT 0
+        );
+        '''
+    )
+
+    cur.execute(
+        '''        
+        CREATE TABLE IF NOT EXISTS "journals" (
+            "created_on" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            "entry" TEXT NOT NULL,
+            "title" TEXT
+        );
+        '''
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
--- mh_lib.py
+++ mh_lib.py
@@ -1,10 +1,10 @@
 import requests
 from random import randint
-import sqlite3
+import psycopg2
+from dotenv import load_dotenv
 import os
+from db_init import initialize_conn
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE =  os.path.join(BASE_DIR, "mentalhealth.db")
 
 def random_xkcd():
     comic_number = randint(0, 3057)
@@ -13,33 +13,35 @@ def random_xkcd():
     return data
 
 def add(data):
-    con = sqlite3.connect(DATABASE)
-    cur = con.cursor()
-    cur.execute("INSERT INTO day(focus, mood, sleep, note) VALUES(?, ?, ?, ?)", data)
-    con.commit()
-    con.close()
+    conn = initialize_conn()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO day(focus, mood, sleep, note) VALUES(%s, %s, %s, %s)", data)
+    conn.commit()
+    cur.close()
+    conn.close()
 
 def get():
-    data_tuples = []
-    con = sqlite3.connect(DATABASE)
-    cur = con.cursor()
-    for row in cur.execute("SELECT * FROM day ORDER BY date"):
-        data_tuples.append(row)
-    con.close()
+    conn = initialize_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM day ORDER BY date")
+    data_tuples = cur.fetchall()
+    cur.close()
+    conn.close()
     return data_tuples
 
 def add_journal(data):
-    con = sqlite3.connect(DATABASE)
-    cur = con.cursor()
-    cur.execute("INSERT INTO journals(entry, title) VALUES(?, ?)", data)
-    con.commit()
-    con.close()
+    conn = initialize_conn()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO journals(entry, title) VALUES(%s, %s)", data)
+    conn.commit()
+    cur.close()
+    conn.close()
 
 def get_journals():
-    data_tuples = []
-    con = sqlite3.connect(DATABASE)
-    cur = con.cursor()
-    for row in cur.execute("SELECT * FROM journals ORDER BY created_on"):
-        data_tuples.append(row)
-    con.close()
+    conn = initialize_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM journals ORDER BY created_on")
+    data_tuples = cur.fetchall()
+    cur.close()
+    conn.close()
     return data_tuples
